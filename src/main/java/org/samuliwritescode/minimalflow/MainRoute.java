912ff32665edfe427793a9db4e12b4d4f87c2fdc
Samuli Penttila <samuli@vaadin.com>
2024-02-14 08:47:41 +0200

    Button with hello world notification

--- src/main/java/org/samuliwritescode/minimalflow/MainRoute.java
+++ src/main/java/org/samuliwritescode/minimalflow/MainRoute.java
@@ -1,6 +1,8 @@
 package org.samuliwritescode.minimalflow;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.router.Route;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @Route("") // <-- Vaadin will make instance of this class to be displayed at root of http://localhost:8080/
 public class MainRoute extends Div {
     public MainRoute() {
-        add("Hello world");
+        add(new Button("Hello world", event -> Notification.show("Well hello world!")));
     }
 
     @SpringBootApplication // <-- So that you may run this as a Spring Boot application
