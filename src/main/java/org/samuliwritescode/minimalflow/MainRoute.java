7f5beb365629a3edf716bbcf7e9339041e05301f
Samuli Penttila <samuli@vaadin.com>
2024-02-14 08:06:28 +0200

    Added comments

--- src/main/java/org/samuliwritescode/minimalflow/MainRoute.java
+++ src/main/java/org/samuliwritescode/minimalflow/MainRoute.java
@@ -5,17 +5,17 @@ import com.vaadin.flow.router.Route;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Route("")
+@Route("") // <-- Vaadin will make this to be displayed at root of http://localhost:8080/
 public class MainRoute extends Div {
     public MainRoute() {
         add("Hello world");
     }
 
-    @SpringBootApplication
+    @SpringBootApplication // <-- So that you may run this as a Spring Boot application
     public static class Application {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args); // <-- So that you may run this directly as a Java application
     }
 }
